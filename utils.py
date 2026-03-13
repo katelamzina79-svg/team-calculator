4f0e05f2bdbb5f8cf05f4b5ac5e7ba0706fc835e
Давид Канаев <dkmozzarella@gmail.com>
2026-03-13 22:19:44 +0300

    Add type checking to subtract

--- utils.py
+++ utils.py
@@ -1,3 +1,5 @@
 # utils.py - Математические функции
 def subtract(a,b):
+	if not isinstance(a, (int, float)) or not isinstance(b, (int, float)):
+		raise TypeError("Аргументы должны быть числами")
 	return a-b
